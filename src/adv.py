155eb9bbe2917d0bb86cf3dfc71b69b9c7474b1f
dmhliu <dmhliu@gmail.com>
2020-09-02 15:19:30 -0700

    add check for valid move

--- src/adv.py
+++ src/adv.py
@@ -29,23 +29,59 @@ def main():
     room['foyer'].s_to = room['outside']
     room['foyer'].n_to = room['overlook']
     room['foyer'].e_to = room['narrow']
+
     room['overlook'].s_to = room['foyer']
     room['narrow'].w_to = room['foyer']
     room['narrow'].n_to = room['treasure']
     room['treasure'].s_to = room['narrow']
+#### my way 
+    room['outside'].set_neighbor('n', room['foyer'])
 
+    room['foyer'].set_neighbor('s', room['outside'])
+    room['foyer'].set_neighbor('n', room['overlook'])
+    room['foyer'].set_neighbor('e', room['narrow'])
+    room['overlook'].set_neighbor('s', room['foyer'])
+    room['narrow'].set_neighbor('w', room['foyer'])
+    room['narrow'].set_neighbor('n', room['treasure'])
+    room['treasure'].set_neighbor('s', room['narrow'])
     # Make a new player object that is currently in the 'outside' room.
+
+    pname = input("Please give your player a name: ")
+    newplayer = Player(name=pname, room=room['outside']) # starts outside by default
+    validinputs =  ['n','s','e','w','q']  
+
+    def debug(player=None):
+        print(f"[DEBUG]rooms dict: {room.keys()}")
+        if player:
+            print(f"[DEBUG]current player is {player}")
+            print(f"[DEBUG]current room and neighbors: \n {player.room} \n {player.room.neighbor}")
     # Write a loop that:
     while True: 
-        pname = input("Please give your player a name: ")
-        newplayer = Player(name=pname) # starts outside by default
     # * Prints the current room name   #assume this means the room that the current player is in.
-        print(f"\n The current player {newplayer.getname()} is in {newplayer.getlocation()}")
+
+        print(f"\n.....{newplayer.getname()} is in {newplayer.getlocationname()}")
 
     # * Prints the current description (the textwrap module might be useful here).
+        print(f"'{newplayer.getlocation().describe()}'")
     # * Waits for user input and decides what to do.
-    #
-    # If the user enters a cardinal direction, attempt to move to the room there.
+    #   
+        rawinput = input("Where to? [n|s|e|w|q] : ")
+        # test input
+        try:  # make it throw and error for fun
+            _ = validinputs.index(rawinput)
+        except ValueError: 
+            print (f"\n Sorry, I didn't understand that! \nPlease Enter one of the following {validinputs}")
+            continue
+        if rawinput == 'q' :
+            print(f"{newplayer} says 'live long and prosper! \/' ")
+            break
+        else:
+            if newplayer.move_to(rawinput):
+                print(f"{newplayer} moved to the {newplayer.getlocationname()}")
+            else:
+                print(f".\n..\n...\n\....yeah, nope. there no room in that direction!")
+                print(f"HINT: try {newplayer.getlocation().get_neighbor()}")
+        # If the user enters a cardinal direction, attempt to move to the room there.
     # Print an error message if the movement isn't allowed.
     #
     # If the user enters "q", quit the game.print
--- src/player.py
+++ src/player.py
@@ -2,14 +2,35 @@
 # currently.
 
 class Player:
-    def __init__(self, name='john doe', room=room['outside']): 
+    def __init__(self, name='john doe', room=None): 
         """create player in a room, default to outside"""
         self.name = name
         self.room = room
+    def __str__(self):
+        return self.name
     def getname(self):
         return self.name
-    def getlocation(self):
+    def getlocationname(self):
         return self.room.getname()
+    def getlocation(self):
+        return self.room
+
+    def __set_room__(self, newroom): # use a private method to set
+        self.room = newroom
+    def move_to(self, direction):
+        """try to move to the room neighboring in {direction} of 
+        the players current room
+        direction : string {n,s,e,w}
+        return : bool True if current room neighbor exists else False
+        """
         
+        if self.room.neighbor[direction]:  #check if room in dir exists
+            self.__set_room__(self.room.neighbor[direction])
+            return True
+        else:
+            return False
+
+
+
 
         
