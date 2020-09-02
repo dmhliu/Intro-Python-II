4e554419760d7df24ab904537b243906914cef18
dmhliu <dmhliu@gmail.com>
2020-09-02 11:29:31 -0700

    hw first pass

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name='john doe', room=room['outside']): 
+        """create player in a room, default to outside"""
+        self.name = name
+        self.room = room
+    def getname(self):
+        return self.name
+    def getlocation(self):
+        return self.room.getname()
+        
+
+        
--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, desc):
+        self.name = name
+        self.description = desc
+        
+        #store neighbors by cardinal direction to.. per spec. not my idea
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+    def __str__(self):
+        return f"Room: {self.name} \n Description: {self.description}"
+    def getname(self):
+        return self.name
+    
