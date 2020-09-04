a034c5f5714eb4ee44a31d06235c0774b7a3087c
dmhliu <dmhliu@gmail.com>
2020-09-03 19:23:15 -0700

    item

---
+++ src/item.py
@@ -0,0 +1,8 @@
+"""define the item class Item can be located in a room or
+ carried by a player
+"""
+class Item:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return self.name
